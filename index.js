6c6280c6e53b629b2e5a87b1a2b832c9c28addce
Gabriel Alabi¡ <you@example.com>
2022-02-23 22:19:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3002;
+const axios = require('axios');
+const cors = require('cors');
+app.use(cors({
+  origin: '*'
+}))
+app.get('/', (req,res,next) => {
+    res.send('holiiii')
+})
+app.get("/:endpoint([\\/\\w\\.-]*)", (req, res, next) => {
+    let endpoint = 'https://api-dolar-argentina.herokuapp.com/api/' + req.params.endpoint;
+    axios.get(endpoint).then(response => {
+    res.json(response.data)}).catch(error =>{
+      res.json(error)
+      }) 
+});
+
+  
+
+app.listen(port);
